4edd366e268a23472df361eed29031530b48d220
Leonardo Croda <lccroda@gmail.com>
2023-07-31 13:51:02 -0300

    fix: adiciona migrações

--- auth-service/src/config/datasource.config.ts
+++ auth-service/src/config/datasource.config.ts
@@ -1,8 +1,8 @@
 import { DataSourceOptions } from 'typeorm';
-
 import { UserSchema } from '../user/infra/db/typeorm/user-schema';
-
 import { config } from 'dotenv';
+import { CreateUserTable1690649242920 } from 'src/user/infra/db/typeorm/migrations/1690649242920-create_user_table';
+import { InsertUserFake1690649278082 } from 'src/user/infra/db/typeorm/migrations/1690649278082-insert_user_fake';
 
 config();
 
@@ -14,4 +14,6 @@ export const dataSourceOptions = {
   password: 'senha_teste',
   database: 'wallet',
   entities: [UserSchema],
+  migrations: [CreateUserTable1690649242920, InsertUserFake1690649278082],
+  migrationsRun: true,
 } as DataSourceOptions;
